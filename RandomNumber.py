89a9c4654e88ca691b9a8ebf71729c0999c20daa
Sahaj1coder2cool3fantastic <65350245+Sahaj1coder2cool3fantastic@users.noreply.github.com>
2021-09-09 11:56:31 +0530

    Add files via upload

--- RandomNumber.py
+++ RandomNumber.py
@@ -1,10 +1,16 @@
 import random
 number=random.randint(1,9)
 chances = 0
-guessNo = input("Enter a number:")
 while chances < 5:
+    number=random.randint(1,9)
+    guessNo = input("Enter a number:")
     if guessNo == number:
         print("Congratulations You Won!")
+        chances = chances + 1
         break
     if guessNo != number:
+        chances = chances + 1
         print("You Lose! The number is:", number)
+        chances = chances + 1
+    if  chances > 5:
+        print("Maximum chances reached!")
